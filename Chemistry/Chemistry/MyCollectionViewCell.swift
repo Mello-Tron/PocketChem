f3d807aecaee085ea5bc94b0f0c1a7d4beaf2b28
Kaitlyn Duarte <kduarte17@lab-sci-304-22.local>
2017-04-26 10:18:33 -0700

    Experimentation with collection view controller.
    Several different possible ways to control views + navigation +
    collection views. Not sure which will be applicable. Runs but doesn’t
    function.

---
+++ Chemistry/Chemistry/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  Chemistry
+//
+//  Created by Kaitlyn E Duarte on 4/26/17.
+//  Copyright © 2017 bleh. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
