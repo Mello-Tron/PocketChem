f35a8714546b4b026a27d303574c8069ba7fdb7a
Kaitlyn Duarte <kduarte17@lab-sci-304-22.local>
2017-04-30 11:31:41 -0700

    Warnings Resolved
    Resolved warnings and changed gray scale colors

--- Chemistry/Chemistry/ViewController.swift
+++ Chemistry/Chemistry/ViewController.swift
@@ -14,31 +14,6 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
     UINavigationControllerDelegate {
     
-    let reuseIdentifier = "cell"
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items.count
-    }
-    // make a cell for each cell index path
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // get a reference to our storyboard cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
-        
-        // Use the outlet in our custom class to get a reference to the UILabel in the cell
-        cell.myLabel.text = self.items[indexPath.item]
-        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
-        
-        return cell
-    }
-    
-    // MARK: - UICollectionViewDelegate protocol
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // handle tap events
-        print("You selected cell #\(indexPath.item)!")
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -100,20 +75,21 @@ class ShadowButton: UIButton {
         layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         layer.masksToBounds = false
         layer.shadowRadius = 1.0
-        layer.shadowOpacity = 0.1
+        layer.shadowOpacity = 0.3
         
         //Rounded
-        layer.cornerRadius = 5
+        layer.cornerRadius = 2.5
     }
 }
 
 class ShadowLabel: UILabel {
     override func awakeFromNib() {
+        //Shadow
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         layer.masksToBounds = false
         layer.shadowRadius = 1.0
-        layer.shadowOpacity = 0.1
+        layer.shadowOpacity = 0.3
     }
 }
 
@@ -139,7 +115,7 @@ class RoundButton: UIButton {
         layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         layer.masksToBounds = false
         layer.shadowRadius = 1.0
-        layer.shadowOpacity = 0.1
+        layer.shadowOpacity = 0.3
     }
     func buttonPressed(){
         print("work?")
