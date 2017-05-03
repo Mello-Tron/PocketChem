3d3623465cc7d8a229adc85d1dca54bfe1a7dcb2
Patrick Herbert <patricktrherbert@gmail.com>
2017-05-03 13:44:45 -0700

    Add Left/Right Swipe to SetUIImageView

--- Chemistry/Chemistry/SetViewController.swift
+++ Chemistry/Chemistry/SetViewController.swift
@@ -3,7 +3,8 @@ import UIKit
 class SetViewController: ViewController{
     //SET VIEW - swipable UIImageView
     @IBOutlet weak var swipeView: UIImageView!
-    let swipeRec = UISwipeGestureRecognizer()
+    let swipeRight = UISwipeGestureRecognizer()
+    let swipeLeft = UISwipeGestureRecognizer()
     
     var setImageList:[String] = ["IUPAC1@0.5x", "IUPAC2@0.5x"]
     let setMaxImages = 1
@@ -12,14 +13,31 @@ class SetViewController: ViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swipeRec.addTarget(self, action: #selector(self.swipedView))
-        swipeView.addGestureRecognizer(swipeRec)
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        swipeRight.addTarget(self, action: #selector(self.swipedView))
+        swipeView.addGestureRecognizer(swipeRight)
+        
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        swipeLeft.addTarget(self, action: #selector(self.swipedView))
+        swipeView.addGestureRecognizer(swipeLeft)
+        
         swipeView.isUserInteractionEnabled = true
     }
     
-    func swipedView() {
-        let tapAlert = UIAlertController(title: "Swiped", message: "You just swiped the swipe view", preferredStyle: UIAlertControllerStyle.alert)
-        tapAlert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
-        self.present(tapAlert, animated: true, completion: nil)
+    func swipedView(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                let tapAlert = UIAlertController(title: "Swiped", message: "You just swiped the view right", preferredStyle: UIAlertControllerStyle.alert)
+                tapAlert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+                self.present(tapAlert, animated: true, completion: nil)
+            case UISwipeGestureRecognizerDirection.left:
+                let tapAlert = UIAlertController(title: "Swiped", message: "You just swiped the view left", preferredStyle: UIAlertControllerStyle.alert)
+                tapAlert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: nil))
+                self.present(tapAlert, animated: true, completion: nil)
+            default:
+                break
+            }
+        }
     }
 }
