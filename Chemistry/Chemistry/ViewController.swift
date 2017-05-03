1ca54f12f3e333ebca5cdf389fa7ad97d0a165c5
Patrick <patricktrherbert@gmail.com>
2017-05-02 19:00:03 -0700

    Add Camera Button to Home View, Add Transformation Functions for Buttons

--- Chemistry/Chemistry/ViewController.swift
+++ Chemistry/Chemistry/ViewController.swift
@@ -21,16 +21,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-    @IBAction func openCameraIdentifyView(_ sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
-                let imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
-                imagePicker.allowsEditing = false
-                self.present(imagePicker, animated: true, completion: nil)
-        }
-    }
     
     @IBAction func openCameraDrawView(_ sender: AnyObject) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
@@ -45,25 +35,64 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBOutlet var plusButton: RoundButton!
     
     @IBAction func plusButtonPress(_ sender: AnyObject) {
-        //let button = RoundButton()
-        
-        //Animate rorate button on touch
-        UIView.animate(withDuration: 3.0) {
-            self.plusButton.transform = CGAffineTransform(rotationAngle: (CGFloat(M_PI)))
+        if (self.plusButton.isPressed) {
+            self.plusButton.isPressed = false
+            rotateButton(angle: (0.0), duration: 0.1, button: self.plusButton)
+            fadeOutButton(duration: 0.1, button: self.cameraButton)
+        } else {
+            self.plusButton.isPressed = true
+            rotateButton(angle: (CGFloat(M_PI)/4.0), duration: 0.1, button: self.plusButton)
+            fadeInButton(duration: 0.1, button: self.cameraButton)
         }
-        
-        //Make button grow then go back to normal
-        UIView.animate(withDuration: 0.6, animations: {
-            self.plusButton.transform = CGAffineTransform(scaleX: 0.6, y:0.6)
-        },
-                       completion:{_ in
-                        UIView.animate(withDuration: 0.6) {
-                            self.plusButton.transform = CGAffineTransform.identity
-                        }
-        })
     }
     
+    @IBOutlet var cameraButton: CameraButton!
+
+    @IBAction func openCameraHomeView(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
     
+}
+
+////////////////////////////////////////
+//             Animations             //
+////////////////////////////////////////
+
+func rotateButton(angle: CGFloat, duration: Double, button: UIButton) {
+    UIView.animate(withDuration: duration) {
+        button.transform = CGAffineTransform(rotationAngle: angle)
+    }
+}
+
+func fadeInButton(duration: Double, button: UIButton) {
+    UIView.animate(withDuration: duration, animations: {
+        button.alpha = 1.0
+    })
+    button.isEnabled = true
+}
+
+func fadeOutButton(duration: Double, button: UIButton) {
+    UIView.animate(withDuration: duration, animations: {
+        button.alpha = 0.0
+    })
+    button.isEnabled = false
+}
+
+func shrinkAndGrowButton(duration: Double, button: UIButton) {
+     UIView.animate(withDuration: duration, animations: {
+     button.transform = CGAffineTransform(scaleX: 0.6, y:0.6)
+     },
+     completion:{_ in
+     UIView.animate(withDuration: duration) {
+     button.transform = CGAffineTransform.identity
+     }
+     })
 }
 
 ////////////////////////////////////////
@@ -72,6 +101,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
 
 class ShadowButton: UIButton {
     override func awakeFromNib() {
+        //Shadow
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        layer.masksToBounds = false
+        layer.shadowRadius = 1.0
+        layer.shadowOpacity = 0.3
+        
+        //Rounded
+        layer.cornerRadius = 2.5
+    }
+}
+
+class CameraButton: UIButton {
+    override func awakeFromNib() {
+        alpha = 0.0
+        isEnabled = false
+        
         //Shadow
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
@@ -103,14 +149,14 @@ class ShadowNavBar: UINavigationBar {
 }
 
 class RoundButton: UIButton {
+    var isPressed = true
+    
     override func awakeFromNib() {
         //Make circular
         layer.cornerRadius = 0.5 * bounds.size.width
         
         //scale image size
         imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        
-        addTarget(self, action: #selector(buttonPressed), for: UIControlEvents.touchUpInside)
         
         //Give Shadow
         layer.shadowColor = UIColor.black.cgColor
@@ -119,27 +165,5 @@ class RoundButton: UIButton {
         layer.shadowRadius = 1.0
         layer.shadowOpacity = 0.3
     }
-    func buttonPressed(){
-        print("work?")
-        
-        //Animate rorate button on touch
-        UIView.animate(withDuration: 3.0) {
-            self.transform = CGAffineTransform(rotationAngle: (CGFloat(M_PI)))
-        }
-        
-        //Make button grow then go back to normal
-        UIView.animate(withDuration: 0.6, animations: {
-            self.transform = CGAffineTransform(scaleX: 0.6, y:0.6)
-        },
-                       completion:{_ in
-                        UIView.animate(withDuration: 0.6) {
-                            self.transform = CGAffineTransform.identity
-                        }
-        })
-        
-        
-    }
-    
-
 }
 
