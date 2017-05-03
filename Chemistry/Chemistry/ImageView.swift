8ff035b323ed7b566e5cb5f3bc41cd0d65168427
Patrick Herbert <patricktrherbert@gmail.com>
2017-05-03 14:19:08 -0700

    Add ShadowUIImageView Class

--- Chemistry/Chemistry/ImageView.swift
+++ Chemistry/Chemistry/ImageView.swift
@@ -1,5 +1,12 @@
 import UIKit
 
-class ShadowImageView: UIImageView {
-    //...
+class ShadowUIImageView: UIImageView {
+    override func awakeFromNib() {
+        //Shadow
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        layer.masksToBounds = false
+        layer.shadowRadius = 1.0
+        layer.shadowOpacity = 0.3
+    }
 }
