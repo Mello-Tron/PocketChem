8687e97858eb3b9dc2ca0f7cc8c77910d0ab2183
KatieShaw11 <kshaw17@lab-sci-304-23.local>
2017-05-03 22:59:13 -0700

    Added files
    Added a bunch of database files, haven’t started the coding part. It
    still runs, so I’m committing while I’m ahead.

---
+++ Chemistry/Chemistry/PocketChem-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  PocketChem-Bridging-Header.h
+//  PocketChem
+//
+//  Created by Kaitlyn J Shaw on 5/3/17.
+//  Copyright © 2017 bleh. All rights reserved.
+//
+
+#ifndef PocketChem_Bridging_Header_h
+#define PocketChem_Bridging_Header_h
+
+#import "FMDB.h"
+
+
+#endif /* PocketChem_Bridging_Header_h */
